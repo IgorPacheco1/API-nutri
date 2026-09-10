9f5ee316b0fcbf9932d4cf302417d6737288faab
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-10 20:38:43 -0300

    Criando a estrutura para a classe para tabela referencial

--- src/main/java/br/com/igorpacheco/apinutri/model/AliementoMacro.java
+++ src/main/java/br/com/igorpacheco/apinutri/model/AliementoMacro.java
@@ -1,4 +1,22 @@
 package br.com.igorpacheco.apinutri.model;
 
-public class AliementoMacro {
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter @Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class AliementoMacro implements Serializable {
+
+    @Column(name = "id_alimento")
+    private Integer idAlimento;
+
+    @Column(name = "id_macros")
+    private Integer idMacros;
 }
