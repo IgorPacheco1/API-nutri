53b2e27956a22af3dad2c83af7fd6dcb24b9bd00
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-10 19:07:54 -0300

    Criação incial do projeto

---
+++ src/main/java/br/com/igorpacheco/apinutri/ApiNutriApplication.java
@@ -0,0 +1,13 @@
+package br.com.igorpacheco.apinutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNutriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiNutriApplication.class, args);
+    }
+
+}
