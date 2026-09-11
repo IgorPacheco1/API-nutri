77e40d88d95bbaaf7594a2fabb39e2f517571ecb
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-10 21:28:52 -0300

    Criação do ID personalizado para tabelas associativas

---
+++ src/main/java/br/com/igorpacheco/apinutri/model/AlimentoMacroId.java
@@ -0,0 +1,25 @@
+package br.com.igorpacheco.apinutri.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AlimentoMacroId implements Serializable {
+
+    @Column(name = "id_alimento")
+    private Integer idAlimento;
+
+    @Column(name = "id_macros")
+    private Integer idMacros;
+
+}
---
+++ src/main/java/br/com/igorpacheco/apinutri/model/AlimentoMicroId.java
@@ -0,0 +1,22 @@
+package br.com.igorpacheco.apinutri.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlimentoMicroId implements Serializable {
+    @Column(name = "id_alimento")
+    private Integer idAlimento;
+
+    @Column(name = "id_micros")
+    private Integer idMicros;
+}
