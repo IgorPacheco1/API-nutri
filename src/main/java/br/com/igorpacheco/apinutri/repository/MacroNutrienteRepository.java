a381686437914799210568400b787604d3bd90ff
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-12 21:44:21 -0300

    Adicionando busca personalizada

--- src/main/java/br/com/igorpacheco/apinutri/repository/MacroNutrienteRepository.java
+++ src/main/java/br/com/igorpacheco/apinutri/repository/MacroNutrienteRepository.java
@@ -3,5 +3,8 @@ package br.com.igorpacheco.apinutri.repository;
 import br.com.igorpacheco.apinutri.model.MacroNutriente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MacroNutrienteRepository extends JpaRepository<MacroNutriente, Integer> {
+    Optional<MacroNutriente> findByNomeMacros(String nomeMacros);
 }
