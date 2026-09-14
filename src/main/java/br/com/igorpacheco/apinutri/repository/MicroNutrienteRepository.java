201bc842e49502489613792d8210ec565405f061
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-13 22:55:38 -0300

    Adicionando a busca personalizada na interface

--- src/main/java/br/com/igorpacheco/apinutri/repository/MicroNutrienteRepository.java
+++ src/main/java/br/com/igorpacheco/apinutri/repository/MicroNutrienteRepository.java
@@ -1,7 +1,12 @@
 package br.com.igorpacheco.apinutri.repository;
 
+import br.com.igorpacheco.apinutri.model.MacroNutriente;
 import br.com.igorpacheco.apinutri.model.MicroNutriente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MicroNutrienteRepository extends JpaRepository<MicroNutriente, Integer> {
+
+    Optional<MicroNutriente> findByNomeMicros(String nomeMicros);
 }
