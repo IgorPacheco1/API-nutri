cfc373bb9fa3a0132105e74527ea46d5d0487a35
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-14 00:10:01 -0300

    formantando o que irá aparecer

--- src/main/java/br/com/igorpacheco/apinutri/service/AlimentoService.java
+++ src/main/java/br/com/igorpacheco/apinutri/service/AlimentoService.java
@@ -1,4 +1,22 @@
 package br.com.igorpacheco.apinutri.service;
 
+import br.com.igorpacheco.apinutri.dto.AlimentoResumoDTO;
+import br.com.igorpacheco.apinutri.repository.AlimentoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AlimentoService {
+    private final AlimentoRepository alimentoRepository;
+
+    public List<AlimentoResumoDTO> buscarPorNome(String nome) {
+        return alimentoRepository.findByNomeAlimentoContainingIgnoreCase(nome)
+                .stream()
+                .map(a -> new AlimentoResumoDTO(a.getIdAlimento(), a.getNomeAlimento()))
+                .toList();
+    }
 }
