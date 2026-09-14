03968bab596c72f7a9174c0e6e36ac65c33eded8
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-14 00:09:00 -0300

    Criando get para busca por nome

--- src/main/java/br/com/igorpacheco/apinutri/controller/AlimentoController.java
+++ src/main/java/br/com/igorpacheco/apinutri/controller/AlimentoController.java
@@ -1,4 +1,24 @@
 package br.com.igorpacheco.apinutri.controller;
 
+import br.com.igorpacheco.apinutri.dto.AlimentoResumoDTO;
+import br.com.igorpacheco.apinutri.service.AlimentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alimentos")
+@RequiredArgsConstructor
+
 public class AlimentoController {
+    private final AlimentoService alimentoService;
+
+    @GetMapping
+    public List<AlimentoResumoDTO> buscar(@RequestParam String nome) {
+        return alimentoService.buscarPorNome(nome);
+    }
 }
