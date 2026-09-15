f481a18c77c06771367bb06e74d745e48a268e2f
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-15 12:21:28 -0300

    Fix: Adicionando como parametro Map

--- src/main/java/br/com/igorpacheco/apinutri/dto/CalculoResponseDTO.java
+++ src/main/java/br/com/igorpacheco/apinutri/dto/CalculoResponseDTO.java
@@ -1,6 +1,7 @@
 package br.com.igorpacheco.apinutri.dto;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
-public record CalculoResponseDTO(BigDecimal totalCalorias, MacrosDTO macros) {
+public record CalculoResponseDTO(BigDecimal totalCalorias, MacrosDTO macros, Map<String, BigDecimal> micros) {
 }
