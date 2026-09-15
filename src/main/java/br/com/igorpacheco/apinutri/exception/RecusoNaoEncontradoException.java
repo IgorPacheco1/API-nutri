ae7da8b0e9529f6592e00c45bb48aede167ebc01
IgorPacheco1 <igopacheco7@gmail.com>
2026-09-15 13:53:12 -0300

    Criando exception personalizada

---
+++ src/main/java/br/com/igorpacheco/apinutri/exception/RecusoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.igorpacheco.apinutri.exception;
+
+public class RecusoNaoEncontradoException extends Exception {
+    public RecusoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
